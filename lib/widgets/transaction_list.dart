9ebefe8819cd9582e7b3ded2ae9b3a4aab6fa7f4
*devMuhammadAfif* <*m.afif9797@gmail.com*>
2021-09-15 21:02:10 +0300

    My Expense

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,44 @@
+import '../widgets/transaction_item.dart';
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: transactions.isEmpty
+            ? LayoutBuilder(builder: (ctx, constraints) {
+                return Column(
+                  // ignore: deprecated_member_use
+                  children: <Widget>[
+                    Text(
+                      'No transactions added yet!',
+                      // ignore: deprecated_member_use
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                        height: constraints.maxHeight * 0.6,
+                        child: Image.asset(
+                          'lib/assets/fonts/images/waiting.png',
+                          fit: BoxFit.cover,
+                        )),
+                  ],
+                );
+              })
+            : ListView(
+                children: transactions
+                    .map((tx) => TransactionItem(
+                        key: ValueKey(tx.id),
+                        transaction: tx,
+                        deleteTx: deleteTx))
+                    .toList()));
+  }
+}
